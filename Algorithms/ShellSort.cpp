c47ab0c51a82bad39901e292cb1be58fc01751cc
Utopia <gejunwei110@gmail.com>
2017-05-24 21:45:00 +0800

    Update ShellSort.cpp

--- Algorithms/ShellSort.cpp
+++ Algorithms/ShellSort.cpp
@@ -13,6 +13,7 @@ void shell_sort(int a[], int len){
     for (int step = len/2; step > 0; step = step / 2) {
         for (int i = 0; i < step; i++) {
             for (int j = i + step; j < len; j = j + step) {
+                //插入排序
                 if (a[j - step] > a[j]) {
                     int insert = a[j];
                     int k = j - step;
@@ -26,6 +27,46 @@ void shell_sort(int a[], int len){
         }
     }
 }
+
+int numberArray[] = {1, 4, 6, 5, 8, 2};
+
+void quickSort(int left, int right){
+    int i, j, pivot;
+   
+    if (left > right) {
+        return;
+    }
+    
+    pivot = numberArray[left];
+    
+    i = left;
+    j = right;
+    
+    while (i != j) {
+        
+        while (numberArray[j] >= pivot && i < j) {
+            j -= 1;
+        }
+        
+        while (numberArray[i] <= pivot && i < j) {
+            i += 1;
+        }
+        
+        if (i < j) {
+            int temp = numberArray[i];
+            numberArray[i] = numberArray[j];
+            numberArray[j] = temp;
+        }
+    }
+    
+    numberArray[left] = numberArray[i];
+    numberArray[i] = pivot;
+    
+    //二分
+    quickSort(left, i - 1);
+    quickSort(i + 1, right);
+}
+
 
 
 int main(int argc, const char * argv[]) {
